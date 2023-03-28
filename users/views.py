5482900ce94391971801c73d69556e645d52b0e8
Srijan Shah <srijanshah09@gmail.com>
2023-03-28 14:16:57 +0530

    register function added

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets 
 from rest_framework import status
 from rest_framework.response import Response
+from .utils import validate_data, register_user
 
 # Create your views here.
 class UserAuthView(viewsets.ViewSet):
@@ -12,10 +13,24 @@ class UserAuthView(viewsets.ViewSet):
         try:
             data = request.data
             print(data)
-            return Response(
-                data = {'status': True, 'message': 'ALL OK',},
-                status = status.HTTP_200_OK,
-            )
+            validate, msg = validate_data(data)
+            if validate:
+                user = register_user(data)
+                if not user:
+                    return Response(
+                        data={'status': False, 'message': "User already exists"}, 
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+ 
+                return Response(
+                    data = {'status': validate, 'message': msg,},
+                    status = status.HTTP_200_OK,
+                )
+            else:
+                return Response(
+                    data = {'status': validate, 'message': msg,},
+                    status = status.HTTP_400_BAD_REQUEST,
+                )
         except Exception as e:
             return Response(
                 data = {'status': False, 'message': str(e),},

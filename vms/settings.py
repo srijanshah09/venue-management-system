6e29f530a81ada09263b08beb2752b7a1851ea2a
Srijan Shah <srijanshah09@gmail.com>
2023-03-30 14:14:28 +0530

    email setup for otp

--- vms/settings.py
+++ vms/settings.py
@@ -146,3 +146,10 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")

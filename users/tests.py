179aa58bd757f71170121130d381e1e615030901
Srijan Shah <srijanshah09@gmail.com>
2024-07-02 18:03:52 +0530

    testcases added

--- users/tests.py
+++ users/tests.py
@@ -6,11 +6,32 @@ from .models import User
 
 class UserModelTestCase(TestCase):
     def test_custom_user_model_exists(self):
-        custom_user = User.objects.all()
+        """    
+        ## test_custom_user_model_exists
+        Checks if there are any instances of the `User` model in the database.
 
+        ### Returns
+        - None
+
+        ### Raises
+        - AssertionError: If the count of instances of the `User` model is not equal to 0.
+        """
+        custom_user = User.objects.all()
         self.assertEqual(custom_user.count(), 0)
 
     def test_string_rep_of_objects(self):
+        """
+        ## test_string_rep_of_objects
+
+        Tests the string representation of objects created using the `User` model.
+
+        ### Returns
+        - None
+
+        ### Raises
+        - AssertionError: If the string representation of the user object does not match the user's name.
+
+        """
         user = User.objects.create(
             username = "admin",
             password = "Q!w2e3r4",
@@ -19,6 +40,18 @@ class UserModelTestCase(TestCase):
         self.assertEqual(str(user), user.name)
         
     def test_user_creation(self):
+        """
+        ## test_user_creation
+        
+        Tests the creation of a new user instance with specified attributes.
+        
+        ### Returns
+        - None
+        
+        ### Raises
+        - AssertionError: If the attributes of the created user instance do not match the expected values.
+
+        """
         user = User.objects.create(username='testuser', name='Test User', email='test@example.com', mobile='1234567890')
         self.assertEqual(user.username, 'testuser')
         self.assertEqual(user.name, 'Test User')
@@ -26,11 +59,35 @@ class UserModelTestCase(TestCase):
         self.assertEqual(user.mobile, '1234567890')
 
     def test_user_creation_invalid_email(self):
+        """
+        ## test_user_creation_invalid_email
+
+        Tests the validation of user creation with an invalid email address.
+        
+        ### Returns
+        - None
+        
+        ### Raises
+        - ValidationError: If the user creation with an invalid email address does not raise a validation error.
+
+        """
         with self.assertRaises(ValidationError):
             user = User(username='testuser', name='Test User', email='invalidemail', mobile='1234567890')
+            user.save()
             user.full_clean()
 
     def test_user_creation_duplicate_mobile(self):
+        """
+        ## test_user_creation_duplicate_mobile
+
+        Tests the validation of user creation with an duplicate mobile number.
+        
+        ### Returns
+        - None
+        
+        ### Raises
+        - ValidationError: If the user creation with an duplicate mobile number, does not raise a validation error.
+        """
         User.objects.create(username='existing_user', name='Existing User', email='existing@example.com', mobile='1234567890')
         with self.assertRaises(ValidationError):
             user = User(username='testuser', name='Test User', email='test@example.com', mobile='1234567890')

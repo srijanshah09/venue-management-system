2e22553d20fa10a7de77ce9acc20652b64d3aa6a
Srijan Shah <srijanshah09@gmail.com>
2024-07-03 15:09:30 +0530

    baker added

--- users/tests/tests_views.py
+++ users/tests/tests_views.py
@@ -1,42 +1,29 @@
 from django.test import TestCase
 from users.models import *
 from http import HTTPStatus
+from model_bakery import baker
 
 
 class HomePageTestCase(TestCase):
     def setUp(self) -> None:
-        user1 = User.objects.create(
-            username="testuser1",
-            name="Test User",
-            email="test1@example.com",
-            mobile="1234567890",
-        )
-        user2 = User.objects.create(
-            username="testuser2",
-            name="Test User",
-            email="test2@example.com",
-            mobile="1234567891",
-        )
+        self.user1 = baker.make(User)
+        self.user2 = baker.make(User)
 
     def test_homepage_returns_correct_response(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "users/index.html")
         self.assertEqual(response.status_code, HTTPStatus.OK)
-    
+
     def test_homepage_returns_list_users(self):
         response = self.client.get("/")
-        self.assertContains(response, "testuser1")
-        self.assertContains(response, "testuser2")
+        self.assertContains(response, self.user1.username)
+        self.assertContains(response, self.user2.username)
+
 
 class DetailsPageTest(TestCase):
     def setUp(self) -> None:
-        self.user = User.objects.create(
-                username="testuser3",
-                name="Test User",
-                email="test3@example.com",
-                mobile="1234567893",
-            )
-    
+        self.user = baker.make(User)
+
     def test_detail_page_correct_response(self):
         response = self.client.get(self.user.get_absolute_url())
         self.assertEqual(response.status_code, HTTPStatus.OK)

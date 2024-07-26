da2ce9df0c4b66fbe1eb23c17e5e058678874f0a
Srijan Shah <srijanshah09@gmail.com>
2024-07-26 13:43:40 +0530

    removed folders not required for traditional deployment

--- scripts/instance_os_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install -y python3-pip
-sudo apt install -y nginx
-sudo apt install -y virtualenv
-sudo apt install -y postgresql
-sudo apt install -y libjpeg-dev zlib1g-dev
--- scripts/stop_app.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash 
-
-sed -i 's/\[]/\["13.201.89.221"]/' /home/ubuntu/venue-management-system/vms/settings.py
-
-sudo service gunicorn stop
-sudo service nginx stop
